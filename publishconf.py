29d559a449f185e1900e4d08853a5f4194dbec49
fernanluis <luisfer439@hotmail.com>
2021-02-15 09:33:47 -0300

    Agregar Disqus

--- publishconf.py
+++ publishconf.py
@@ -14,6 +14,7 @@ SITEURL = 'https://fernanluis.github.io'
 RELATIVE_URLS = False
 
 GOOGLE_ANALYTICS = "G-VXYBJFPECC"
+DISQUS_SITENAME = "https-fernanluis-github-io"
 
 # FEED_ALL_ATOM = 'feeds/all.atom.xml'
 # CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
